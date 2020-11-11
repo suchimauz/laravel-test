c6ee50c570fed6fb8e8d447ab003afc02a254502
suchimauz <alexey.fedulov98@gmail.com>
2020-11-11 11:16:18 +0300

    At time comment

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\EpisodeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +15,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group(['prefix' => 'api'], function () {
+ //   Route::group(['prefix' => 'episodes'], function () {
+ //       Route::get('/', [EpisodeController::class, 'index'])
+ //   });
+
+
 });
